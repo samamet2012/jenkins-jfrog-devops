41ce1e82db074a522ce48e9cb4c8d2e99bcbd86a
Samuel <samamet2012@gmail.com>
2024-12-17 06:54:03 -0600

    code

--- ami-jenkins-jfrog/deploysteps.sh
+++ ami-jenkins-jfrog/deploysteps.sh
@@ -20,3 +20,36 @@ ctrl + c
 
 rm -rf *.jar
 
+### QA deployment steps for nodejs app
+
+1- downloand artifact
+
+curl -uadmin:APPrXaxbjjxRxtvSeVLxsnPmfv -O "http://54.226.16.132:8081/artifactory/nodejs-app/<TARGET_FILE_PATH>"
+
+## Replace this <TARGET_FILE_PATH> with your artifact
+
+2- Extract package
+
+tar -xzf <artifact> #replace <artifact> with downloaded file
+
+3- Go to the source file 'package'
+
+cd package
+
+4- Install Dependencies
+
+npm install
+
+5- Bring up the app
+
+npm start
+
+6- Access app on port 8081
+
+7- Kill app
+
+ctrl + c
+
+8- Clean up 
+
+rm -rf *.tgz
